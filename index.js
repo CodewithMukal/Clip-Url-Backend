2ed7a4803ae676b849bef81ac320fa448d8d7d1a
CodewithMukal <mukal.github@gmail.com>
2025-07-16 00:18:35 +0530

    minor change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ connectDB();
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
-app.use(cors({credentials: true,origin:'http://localhost:5173'}))
+app.use(cors({credentials: true,origin:'https://clipurlx.vercel.app'}))
 
 
 app.get("/",(req,res)=>
